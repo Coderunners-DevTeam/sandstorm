c188dd9187981074c3777bc97b1e8fe9baa52244
hdi10 <hdastekin@gmail.com>
2025-07-14 19:54:01 +0200

    funktionierende sand effekt

--- Scripts/World.cs
+++ Scripts/World.cs
@@ -1,38 +1,61 @@
+/*
+*	Mein SandStrom Game
+*	Harun Dastekin 2025
+*/
+
+
 using Godot;
 using System;
 
 public partial class World : Node2D
 {
-	private const int WIDTH = 256;
-	private const int HEIGHT = 256;
+	////////////////////////////////////////////////////////////////////////////
+
+	private int WIDTH;
+	private int HEIGHT;
 
 	private Image image;
 	private ImageTexture texture;
 	private Sprite2D sprite;
 
-	private Color[,] worldData = new Color[WIDTH, HEIGHT];
+	private Color[,] worldData;
 	private Color sandColor = Colors.Yellow;
 	private Color emptyColor = Colors.Black;
 
 	private bool needsUpdate = true;
 
+
+	////////////////////////////////////////////////////////////////////////////
+	//////              _Ready                                         ///////
+	/////////////////////////////////////////////////////////////////////////
+
 	public override void _Ready()
 	{
-	image = Image.CreateEmpty(WIDTH, HEIGHT, false, Image.Format.Rgba8);
-		image.Fill(emptyColor);
+		Vector2 screenSize = GetViewport().GetVisibleRect().Size;
+		WIDTH = (int)(screenSize.X / 5);  //  Zoom-Faktor
+		HEIGHT = (int)(screenSize.Y / 5);
 
-		texture = ImageTexture.CreateFromImage(image);
+		// Welt vorbereiten
+	worldData = new Color[WIDTH, HEIGHT];
+
+	image = Image.CreateEmpty(WIDTH, HEIGHT, false, Image.Format.Rgba8);
+	image.Fill(emptyColor);
+
+	texture = ImageTexture.CreateFromImage(image);
+
 		sprite = new Sprite2D
 		{
 			Texture = texture,
-			Position = Vector2.Zero,
-			Scale = new Vector2(4, 4)
+			Scale = new Vector2(5, 5) // Zoomfaktor (sichtbar machen)
 		};
 		AddChild(sprite);
 
+		// Zentrieren
+		sprite.Position = (GetViewport().GetVisibleRect().Size / 2f) - ((new Vector2(WIDTH, HEIGHT) * sprite.Scale) / 2f);
+
 		ClearWorld();
 
-		// Testpixel
+		// Testpixel (Sandblock oben links)
 		for (int x = 0; x < 10; x++)
 		{
 			for (int y = 0; y < 10; y++)
@@ -43,16 +66,28 @@ public partial class World : Node2D
 		needsUpdate = true;
 	}
 
+	////////////////////////////////////////////////////////////////////////////
+	//////              ClearWorld                                     ///////
+	/////////////////////////////////////////////////////////////////////////
+
 	private void ClearWorld()
 	{
 		for (int x = 0; x < WIDTH; x++)
+		{
 			for (int y = 0; y < HEIGHT; y++)
+			{
 				worldData[x, y] = emptyColor;
+			}
+		}
 	}
+
+	////////////////////////////////////////////////////////////////////////////
+	//////              _Process                                     ///////
+	/////////////////////////////////////////////////////////////////////////
 
 	public override void _Process(double delta)
 	{
-		HandleMouseInput();
+		HandleTouchInput();
 		UpdateSimulation();
 
 		if (needsUpdate)
@@ -63,13 +98,17 @@ public partial class World : Node2D
 		}
 	}
 
-	private void HandleMouseInput()
+   ////////////////////////////////////////////////////////////////////////////
+   //////              HandleTouchInput                                     ///////
+   /////////////////////////////////////////////////////////////////////////
+
+	private void HandleTouchInput()
 	{
-		if (Input.IsMouseButtonPressed(MouseButton.Left))
+		if (Input.IsActionPressed("ui_touch") || Input.IsMouseButtonPressed(MouseButton.Left))
 		{
-			Vector2 mousePos = GetLocalMousePosition();
-			int x = (int)mousePos.X;
-			int y = (int)mousePos.Y;
+			Vector2 touchPos = GetLocalMousePosition();
+			int x = (int)(touchPos.X / sprite.Scale.X);
+			int y = (int)(touchPos.Y / sprite.Scale.Y);
 
 			if (x >= 0 && x < WIDTH && y >= 0 && y < HEIGHT)
 			{
@@ -79,6 +118,9 @@ public partial class World : Node2D
 		}
 	}
 
+   ////////////////////////////////////////////////////////////////////////////
+   //////              UpdateSimulation                                ///////
+   /////////////////////////////////////////////////////////////////////////
 	private void UpdateSimulation()
 	{
 		bool changed = false;
@@ -97,8 +139,14 @@ public partial class World : Node2D
 		}
 
 		if (changed)
+		{
 			needsUpdate = true;
+		}
 	}
+
+	   ////////////////////////////////////////////////////////////////////////////
+	   //////              UpdateImage                                     ///////
+	   /////////////////////////////////////////////////////////////////////////
 
 	private void UpdateImage()
 	{
